f9a7ff2f097ecc2c1a27d7701cea52ad2a72f587
Kurt Duyck <kurt.duyck@vives.be>
2025-01-08 18:11:24 +0100

    Run speedlify
    *	modified:   CHANGELOG.md
    *	modified:   _data/sites/sample.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.2] - 2024-09-17
+### Changed
+- Speedlify 891
+
 ## [1.1.1] - 2023-11-30
 ### Changed
 - RTM notes
--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,11 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "891_", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://octoplus.vives.be/"
+		"https://www.891.be/",
+	        "https://www.vives.be/"
 	]
 };
